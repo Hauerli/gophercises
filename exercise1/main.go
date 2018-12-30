9040f9a5d2bdc2f31895917cabc6d1539f56e73e
Hauerli <Yb4xNXHDVk2oQ>
2018-12-30 13:05:41 +0100

    finished Part1

--- exercise1/main.go
+++ exercise1/main.go
@@ -3,27 +3,79 @@ package main
 import (
 	"bufio"
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"io"
 	"log"
 	"os"
 )
 
+// TrueAnswer - Answers during Quiz
+var TrueAnswer int
+
+// FalseAnswer - Answers during Quiz
+var FalseAnswer int
+
+// Print the results
+func finalstats() {
+	fmt.Println("-------------------------------------")
+	fmt.Println("You finished the Quiz:")
+	fmt.Println("Correct answers: ", TrueAnswer)
+	fmt.Println("Wrong answers: ", FalseAnswer)
+}
+
+// Print the Question
+func printquest(qu string) {
+	fmt.Println("What is ", qu, " ?")
+}
+
+// Create the Scanner for Terminal input and return input
+func importAnswer() string {
+	// create new scanner for terminal input
+	scanner := bufio.NewScanner(os.Stdin)
+	// catch error for scanner
+	if scanner.Err() != nil {
+		log.Fatal(scanner.Err())
+	}
+	// check the scan
+	scanner.Scan()
+	panswer := scanner.Text()
+	return panswer
+
+}
+
+// compare answers
+func compareAnswer(qanswer string, panswer string) {
+	if panswer == qanswer {
+		TrueAnswer++
+	} else {
+		FalseAnswer++
+	}
+}
+
 func main() {
+
+	// creating flag for filepath
+	var fpath = flag.String("path", "problems.csv", "Path of File")
+	flag.Parse()
+
 	// import file
-	file, err := os.Open("problems.csv")
+	file, err := os.Open(*fpath)
 	// if file not found - error
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// create new reader buffer to import file recods
-	r := csv.NewReader(bufio.NewReader(file))
+	reader := csv.NewReader(bufio.NewReader(file))
 	// go over records
+
 	for {
-		record, err := r.Read()
+		record, err := reader.Read()
 		// break if EndOfFile is reached
 		if err == io.EOF {
+			// call finalstats to tell correct and wrong answers
+			finalstats()
 			break
 		}
 		// error is something is wrong
@@ -32,9 +84,15 @@ func main() {
 		}
 
 		question := record[0]
-		answer := record[1]
+		qanswer := record[1]
 
-		fmt.Println(question, " ", answer)
+		// call question printer
+		printquest(question)
+		// import person answer
+		panswer := importAnswer()
+
+		// compare answers
+		compareAnswer(qanswer, panswer)
 
 	}
 }
