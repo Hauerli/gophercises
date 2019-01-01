0f659cf00ba40aeea4298279c0192811de85afed
Hauerli <Yb4xNXHDVk2oQ>
2019-01-01 15:44:24 +0100

    v2

--- exercise2/urlshort/handler.go
+++ exercise2/urlshort/handler.go
@@ -1,8 +1,17 @@
 package urlshort
 
 import (
+	"log"
 	"net/http"
+
+	"gopkg.in/yaml.v2"
 )
+
+// Config YAML struct
+type Config struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
+}
 
 // MapHandler will return an http.HandlerFunc (which also
 // implements http.Handler) that will attempt to map any
@@ -44,10 +53,30 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 // a mapping of paths to urls.
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
 
-	// needto create yaml Parser
-	// add yaml into map
-	// call mapHandler
+	parsedYAML := parseYAML(yml)
+	urlMAP := buildMAP(parsedYAML)
+	return MapHandler(urlMAP, fallback), nil
 
-	// TODO: Implement this...
-	return nil, nil
+}
+
+func parseYAML(yml []byte) []Config {
+
+	var parsedYAML []Config
+	err := yaml.Unmarshal(yml, &parsedYAML)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return parsedYAML
+
+}
+
+func buildMAP(parsedYAML []Config) map[string]string {
+
+	urlMAP := make(map[string]string)
+
+	for _, line := range parsedYAML {
+		urlMAP[line.Path] = line.URL
+	}
+	return urlMAP
+
 }
