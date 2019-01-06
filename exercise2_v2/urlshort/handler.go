8e8e9c7367ab21ef4175ff051797f4832afdd325
Hauerli <Yb4xNXHDVk2oQ>
2019-01-06 21:22:31 +0100

    v2 final

---
+++ exercise2_v2/urlshort/handler.go
@@ -0,0 +1,69 @@
+package urlshort
+
+import (
+	"net/http"
+
+	"gopkg.in/yaml.v2"
+)
+
+type set struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
+}
+
+// MapHandler will return an http.HandlerFunc (which also
+// implements http.Handler) that will attempt to map any
+// paths (keys in the map) to their corresponding URL (values
+// that each key in the map points to, in string format).
+// If the path is not provided in the map, then the fallback
+// http.Handler will be called instead.
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		url, ok := pathsToUrls[path]
+		if ok {
+			http.Redirect(w, r, url, 301)
+		} else {
+			fallback.ServeHTTP(w, r)
+		}
+	}
+}
+
+// YAMLHandler will parse the provided YAML and then return
+// an http.HandlerFunc (which also implements http.Handler)
+// that will attempt to map any paths to their corresponding
+// URL. If the path is not provided in the YAML, then the
+// fallback http.Handler will be called instead.
+//
+// YAML is expected to be in the format:
+//
+//     - path: /some-path
+//       url: https://www.some-url.com/demo
+//
+// The only errors that can be returned all related to having
+// invalid YAML data.
+//
+// See MapHandler to create a similar http.HandlerFunc via
+// a mapping of paths to urls.
+func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	parsedYaml, err := parseYAML(yml)
+	if err != nil {
+		return nil, err
+	}
+	pathMap := buildMap(parsedYaml)
+	return MapHandler(pathMap, fallback), nil
+}
+
+func parseYAML(data []byte) ([]set, error) {
+	var sets []set
+	err := yaml.Unmarshal(data, &sets)
+	return sets, err
+}
+
+func buildMap(parsedYaml []set) map[string]string {
+	var urlpath = make(map[string]string)
+	for _, val := range parsedYaml {
+		urlpath[val.Path] = val.URL
+	}
+	return urlpath
+}
